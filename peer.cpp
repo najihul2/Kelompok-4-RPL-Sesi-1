a59b3f7aef2eb564a93d3c05e74a13cbbc13e19e
FarrelJonata <farreljonata123@gmail.com>
2025-10-09 22:17:28 +0700

    Update peer.cpp

--- peer.cpp
+++ peer.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main (){
-
+void tampilkanMenu() {
+    cout << "=== MENU TABUNGAN ===" << endl;
+    cout << "1. Setor Uang" << endl;
+    cout << "2. Tarik Uang" << endl;
+    cout << "3. Lihat Saldo" << endl;
+    cout << "4. Keluar" << endl;
+    cout << "Pilih menu: ";
+    }
     
     cout << "Pilihan tidak valid!" << endl;
     return 0;
