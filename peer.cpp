651b491eb6f85969de1c5dfbc3108a161c2549a7
Najihul Wildan Abqory <najihulwildan02@gmail.com>
2025-10-09 22:20:28 +0700

    Update peer.cpp

--- peer.cpp
+++ peer.cpp
@@ -9,6 +9,21 @@ void tampilkanMenu() {
     cout << "4. Keluar" << endl;
     cout << "Pilih menu: ";
     }
+void setorUang(int &saldo, int jumlah) {
+    saldo += jumlah;
+    cout << "Berhasil menyetor Rp" << jumlah << endl;
+}
+void tarikUang(int &saldo, int jumlah) {
+    if (jumlah > saldo) {
+        cout << "Saldo tidak mencukupi!" << endl;
+    } else {
+        saldo -= jumlah;
+        cout << "Berhasil menarik Rp" << jumlah << endl;
+    }
+}
+void lihatSaldo(int saldo) {
+    cout << "Saldo anda saat ini: Rp" << saldo << endl;
+}
     
     cout << "Pilihan tidak valid!" << endl;
     return 0;
